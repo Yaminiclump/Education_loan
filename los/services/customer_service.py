df9bb113d6eb4e91aff5d6e1eb45aebe261b0f67
Anjum Khan <anjum.khan@kuhoo.com>
2022-02-07 16:24:33 +0530

    added contact service

--- los/services/customer_service.py
+++ los/services/customer_service.py
@@ -410,3 +410,20 @@ def update_customer(req_data):
         response_obj = get_response("generic_error_2")
         logger.info("response: %s", response_obj)
     return response_obj
+
+
+def customer_contact(req_data):
+    response_obj = None
+    try:
+        logger.info("request: %s", req_data)
+        if req_data:
+            customer = None
+            if hasattr(req_data, 'customer'):
+                customer = req_data.customer
+                logger.info("response: %s", customer)
+                
+    except Exception as e:
+        logger.exception("Exception: ")
+        response_obj = get_response("generic_error_2")
+        logger.info("response: %s", response_obj)
+    return response_obj
--- los/urls.py
+++ los/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('customer/create', views.customer_create, name='customer_create'),
     path('customer/update', views.customer_update, name='customer_update'),
+    path('customer/contact', views.customer_contact, name='customer_contact'),
 ]
