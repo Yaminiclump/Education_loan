c46478da65f4e2940e744a924666e17f501d5a2c
Anjum Khan <anjum.khan@kuhoo.com>
2022-02-18 16:50:06 +0530

    update employment testcases

--- los/tests/__init__.py
+++ los/tests/__init__.py
@@ -2,3 +2,4 @@ from .test_customer_service import *
 from .test_customer_view import *
 from .test_contact_service import *
 from .test_employment_service import *
+from .test_employment_view import *
