58c7348aea4c239c3d9385f2809afda8cd67d0d3
Yogendra Goyal <Yogendra.Goyal@kuhoo.com>
2022-02-25 19:27:58 +0530

    code review - made one test case correct

--- los/tests/test_education_view.py
+++ los/tests/test_education_view.py
@@ -8,6 +8,7 @@ from django.urls import reverse
 from los.models import Customer
 from los.models import Education
 from los.status_code import Statuses
+from los.tests.test_education_service import customer_insert
 
 logger = logging.getLogger("django")
 
@@ -15,67 +16,31 @@ logger = logging.getLogger("django")
 @pytest.mark.django_db
 class TestEducationCreateView:
 
-    def test_create_education_view_GET(self, client):
+    def test_create_education_view_get(self, client):
 
         url = reverse('education_create')
         response = client.get(url)
         logger.info("response for get method : %s", response)
         assert response.status_code == Statuses.test_get['status_code']
 
-    def test_create_education_view_POST(self, client):
+    def test_create_education_view_post(self, client):
         current_time = django.utils.timezone.now()
         logger.debug("current_time india: %s", current_time)
-        create_customer = Customer(
-            salutation=1,
-            first_name="abc",
-            middle_name="",
-            last_name="",
-            gender=1,
-            date_of_birth="2019-10-25",
-            relation_with_applicant=0,
-            marital_status=1,
-            father_first_name="abc",
-            father_middle_name="abc",
-            father_last_name="",
-            mother_first_name="",
-            mother_middle_name="",
-            mother_last_name="",
-            spouse_first_name="",
-            spouse_middle_name="",
-            spouse_last_name="",
-            no_of_family_members=4,
-            household_income_monthly=5000,
-            status=1,
-            creation_date=current_time,
-            creation_by="System"
-        )
-        create_customer.save()
+        create_customer = customer_insert()
         data = {
             "customer": {
                 "customer_id": create_customer.id,
                 "educations": [
                     {
                         "institute_id": "1",
-                        "institute_name": "string",
-                        "course_id": "0",
-                        "course_name": "string",
-                        "stream_id": "0",
-                        "stream_name": "string",
-                        "start_month_year": "2022-05-11",
-                        "end_month_year": "2022-11-01",
-                        "marks": "10",
-                        "max_marks": "0",
-                        "marks_type": "string",
-                        "duration_months": "0",
-                        "course_type": "string"
-                    },
-                    {
-                        "marks_type": "string",
-                        "marks": "10"
-                    },
-                    {
-                        "course_type": "string",
-                        "duration": "0"
+                        "course_id": "1",
+                        "stream_id": "1",
+                        "end_month_year": "2022-11",
+                        "marks": "93",
+                        "max_marks": "100",
+                        "marks_type": "percentage",
+                        "duration_months": "48",
+                        "course_type": "full_time"
                     }
                 ]
 
